f20c375774f285964db464936269943317417869
victornavelino <victornavelino@gmail.com>
2021-07-29 19:41:08 -0300

    if agregado

--- public/js/modulos/proyectos.js
+++ public/js/modulos/proyectos.js
@@ -1,27 +1,29 @@
 import Swal from "sweetalert2";
 import axios from "axios";
 const btnEliminar = document.querySelector('#eliminar-proyecto');
-btnEliminar.addEventListener('click', () => {
-    Swal.fire({
-        title: 'Esta Seguro?',
-        text: "Esta operacion es ireversible!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Si, Eliminar!',
-        cancelButtonText: 'Cancelar'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            Swal.fire(
-                'Eliminado!',
-                'Su archivo fue eliminado.',
-                'success'
-            );
-            //redirigir al inicio
-            setTimeout(()=>{
-                window.location.href ='/'
-            }, 2000);
-        }
+if (btnEliminar) {
+    btnEliminar.addEventListener('click', () => {
+        Swal.fire({
+            title: 'Esta Seguro?',
+            text: "Esta operacion es ireversible!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Eliminar!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Eliminado!',
+                    'Su archivo fue eliminado.',
+                    'success'
+                );
+                //redirigir al inicio
+                setTimeout(() => {
+                    window.location.href = '/'
+                }, 1000);
+            }
+        })
     })
-})
+}
