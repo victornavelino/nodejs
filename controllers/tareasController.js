320cbe94be36c4dc23ea7d8ed3ca77fd079436bd
victornavelino <victornavelino@gmail.com>
2021-08-19 20:27:21 -0300

    tareas

--- controllers/tareasController.js
+++ controllers/tareasController.js
@@ -1,3 +1,10 @@
-exports.agregarTarea = (req, res) => {
-    res.send('Enviado');
+const Proyectos = require('../models/Proyectos');
+const Tareas = require('../models/Tareas');
+
+
+exports.agregarTarea = async (req, res) => {
+    //obtenemos el proyecto actual
+    const proyecto = await Proyectos.findOne({where: {url: req.params.url}});
+    console.log(proyecto);
+    console.log(req.body);
 }
