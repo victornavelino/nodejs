e67d13e754e14dfc66dbf8f56e28f79c3519db2d
victornavelino <victornavelino@gmail.com>
2021-09-09 20:22:02 -0300

    alertas

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -18,8 +18,9 @@ exports.crearCuenta = async (req, res) => {
         });
         res.redirect('/iniciar-sesion')
     } catch (error) {
+        req.flash('error', error.errors.map(error => error.message));
         res.render('crearCuenta', {
-            errores: error.errors,
+            errores: req.flash(),
             nombrePagina: 'Crear Cuenta en UpTask'
 
         })
--- index.js
+++ index.js
@@ -37,6 +37,7 @@ app.use((req,res,next)=>{
     res.locals.vardump = helpers.vardump;
     res.locals.anio = 2019;
     res.locals.title= "PROYECTOSOK"
+    res.locals.mensajes = req.flash();
     next();
 });
 
